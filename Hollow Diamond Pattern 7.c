116c93f5d0ab0083c29f8d1fb0cde04dac01dc24
balakishore6666-lab <balakishore6666@gmail.com>
2026-02-06 13:33:34 +0530

    Create Hollow Diamond Pattern 7.c

---
+++ Hollow Diamond Pattern 7.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int i,j;
+
+    for(i=1;i<=n;i++) {
+        for(j=1; j<=n-i;j++) {
+            printf(" ");
+        }
+        for(j=1;j<=2*i-1; j++) {
+            if(j==1||j==2*i-1)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+
+    for(i=n-1;i>=1;i--) {
+        for(j=1;j<=n-i;j++) {
+            printf(" ");
+        }
+        for(j=1;j<=2*i-1;j++) {
+            if(j==1||j==2*i-1)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+        
+    return 0;
+}
