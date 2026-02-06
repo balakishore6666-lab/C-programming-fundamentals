a73e4232592471d1397e0a339d211e29a5a1938f
balakishore6666-lab <balakishore6666@gmail.com>
2026-02-06 12:17:42 +0530

    Implement isThree function to check for three divisors

---
+++ THREE DIVISORS 3.c
@@ -0,0 +1,20 @@
+class Solution 
+{
+public:
+    bool isThree(int n) 
+    {
+        if(n<4) return false;
+        int root=sqrt(n);
+        if(root*root!=n)
+        return false;
+        for(int i=2;i*i<=root;i++)
+        {
+            if(root%i==0)
+            {
+            return false;
+            }
+        }
+        return true;
+    }
+   
+};
