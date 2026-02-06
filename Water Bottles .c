3e858972ac30caa2127910583389ba991205dbcf
balakishore6666-lab <balakishore6666@gmail.com>
2026-02-06 12:31:56 +0530

    Implement water bottle exchange solution

---
+++ Water Bottles .c
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int total=numBottles;
+        int empty=numBottles;
+        while(empty>=numExchange)
+        {
+            int newBottle=empty/numExchange;
+            total=total+newBottle;
+            empty=newBottle+(empty%numExchange);
+        }
+        return total;
+    }
+};
